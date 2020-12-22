111c6286b528e802fe27c2256e002cef131613e9
Aditya Pratap Singh <58381523+devmrfitz@users.noreply.github.com>
2020-12-22 20:57:09 +0530

    Update activity.py

--- activity.py
+++ activity.py
@@ -92,8 +92,10 @@ class TicTacToe(activity.Activity):
 
         vbox.pack_start(self.turnview, True, True, 0)
         grid = Gtk.Grid()
-        grid.show()
+        grid.halign = Gtk.Align.CENTER
+        vbox.halign = Gtk.Align.CENTER        
         vbox.pack_start(grid, True, True, 0)
+        grid.show()
 
         for _ in range(3):
             for __ in range(3):
@@ -101,8 +103,7 @@ class TicTacToe(activity.Activity):
                 self.list[_][__].connect("clicked", self.click, _, __)
 
         grid.attach(self.list[0][0], 0, 0, 1, 1)
-        grid.halign = Gtk.Align.CENTER
-        vbox.halign = Gtk.Align.CENTER
+
 
         grid.attach_next_to(
             self.list[0][1], self.list[0][0], Gtk.PositionType.RIGHT, 1, 1)
