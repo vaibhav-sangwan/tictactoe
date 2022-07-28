5c698c5208f82677446aa49992fbf945d611502f
sparshg <43041139+sparshg@users.noreply.github.com>
2022-07-28 15:13:30 +0530

    Fix activity not opening with sugar.

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import pygame as pg
 import g
 from frame import Frame
 import sys
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
 
 # The main controller
 class Main:
-    def __init__(self):
-        pg.init()
-
-        # self.win = pg.display.set_mode((WIDTH, HEIGHT))
+    def __init__(self, journal=True):
+        self.journal = journal
         self.running = True
         self.canvas = None
-
 
     def set_canvas(self, canvas):
         self.canvas = canvas
@@ -31,8 +32,8 @@ class Main:
                 pg.display.set_mode(event.size, pg.RESIZABLE)
                 break
             if event.type == pg.MOUSEBUTTONUP:
-                # if self.canvas is None:
-                #     self.canvas.grab_focus()
+                if self.canvas is not None:
+                    self.canvas.grab_focus()
                 self.frame.detect_click(pg.mouse.get_pos())
 
     def draw(self):
@@ -45,14 +46,23 @@ class Main:
 
     # The main loop
     def run(self):
+        for event in pg.event.get():
+            if event.type == pg.VIDEORESIZE:
+                pg.display.set_mode(event.size, pg.RESIZABLE)
+                break
         g.init()
+        if self.canvas is not None:
+            self.canvas.grab_focus()
+
         self.frame = Frame(self, (g.WIDTH/2, g.HEIGHT/2))
         self.clock = pg.time.Clock()
         while self.running:
+            if self.journal:
+                # Pump GTK messages.
+                while Gtk.events_pending():
+                    Gtk.main_iteration()
+
             self.check_events()
-            # if self.win is None:
-            #     self.win = pg.display.get_surface()
-            # else:
             self.draw()
             self.clock.tick(g.FPS)
         pg.display.quit()
@@ -62,6 +72,6 @@ class Main:
 
 # Test if the script is directly ran
 if __name__ == "__main__":
-    main = Main()
+    main = Main(journal=False)
     pg.display.set_mode((1024, 768))
     main.run()
